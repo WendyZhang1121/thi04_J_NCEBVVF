667f54b408742752241df273a8da14557cc661ee
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 23:12:59 +0100

    Creation

---
+++ src/thi04_J_NCEBVVF/SocketReader.java
@@ -0,0 +1,51 @@
+package thi04_J_NCEBVVF;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+//Thread-safe class
+public final class SocketReader implements Runnable {
+	
+	private final Socket socket;
+	private final BufferedReader in;
+	
+	private volatile boolean done = false; 
+	private final Object lock = new Object();
+
+	public SocketReader(String host, int port) throws IOException { 
+		this.socket = new Socket(host, port);
+		this.in = new BufferedReader( new InputStreamReader(this.socket.getInputStream()) );
+	}
+	
+	//Only one thread can use the socket at a particular time 
+	@Override public void run() {
+		try {
+			synchronized (lock) {
+				readData();
+			}
+		} catch (IOException ie) { 
+			// Forward to handler
+		} 
+	}
+
+	public void readData() throws IOException {
+		String string;
+		while (!done && (string = in.readLine()) != null) {
+			//Blocks until end of stream (null) 
+		}
+	}
+	
+	public void shutdown() { 
+		done = true;
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SocketReader reader = new SocketReader("somehost", 25); 
+		Thread thread = new Thread(reader);
+		thread.start();
+		Thread.sleep(1000);
+		reader.shutdown(); // Shutdown the thread 
+		}
+}
